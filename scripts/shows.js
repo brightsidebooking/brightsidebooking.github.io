b29e41c57639b4d8981329263d741658247aa775
Brandon Scott <brandongregoryscott@gmail.com>
2018-02-27 16:46:09 -0500

    Fixed spelling error _again_

--- scripts/shows.js
+++ scripts/shows.js
@@ -3,41 +3,41 @@ shows.push({
     "title" : null,
     "bands" : ["Worries", "Honeyfitz", "The Ok Oks", "Sun Not Yellow"],
     "date"  : "01.18.2018",
-    "flierImgPath": "img/flyers/01182018.jpg",
+    "flierImgPath": "img/fliers/01182018.jpg",
     "fbEventUrl" : "http://www.facebook.com/events/174086326525325/"
 },
 {
     "title" : null,
     "bands" : ["Fire Is Motion", "Tyler Burkhart", "Weak Lungs", "The Orderlies"],
     "date"  : "01.25.2018",
-    "flierImgPath": "img/flyers/01252018.jpg",
+    "flierImgPath": "img/fliers/01252018.png",
     "fbEventUrl" : "http://www.facebook.com/events/137919250216027"
 },
 {
     "title" : null,
     "bands" : ["Seasonal", "Colourshow", "Tyler Burkhart"],
     "date"  : "02.10.2018",
-    "flierImgPath": "img/flyers/02102018.jpg",
+    "flierImgPath": "img/fliers/02102018.jpg",
     "fbEventUrl" : "http://www.facebook.com/events/203211766920586/"
 },
 {
     "title" : "Cory Paternoster Record Release with Analog Watts and Lupia",
     "bands" : ["Cory Paternoster", "Analog Watts", "Lupia"],
     "date"  : "02.23.2018",
-    "flierImgPath": "img/flyers/02232018.jpg",
+    "flierImgPath": "img/fliers/02232018.jpg",
     "fbEventUrl" : "http://www.facebook.com/events/1854746461485055/"
 },
 {
     "title" : null,
     "bands" : ["Vicious Blossom", "Drivers Ed", "Dunce Cap", "Local News Legend"],
     "date"  : "03.08.2018",
-    "flierImgPath": "img/flyers/03082018.jpg",
+    "flierImgPath": "img/fliers/03082018.jpg",
     "fbEventUrl" : "http://www.facebook.com/events/888586337985741/"
 },
 {
     "title" : null,
     "bands" : ["Sleepy Limbs", "Pale Shade", "Middlekid", "The Ok Oks"],
     "date"  : "03.16.2018",
-    "flierImgPath": "img/flyers/03162018.jpg",
+    "flierImgPath": "img/fliers/03162018.jpg",
     "fbEventUrl" : "http://www.facebook.com/events/317908582050961/"
 });
