cfbff6680970f8adba8ddb8b88b5af7b9276957e
Josh Peters <jpeters@listrak.com>
2018-02-27 15:36:49 -0500

    made updating past/upcomming shows dynamic

---
+++ scripts/updates.js
@@ -0,0 +1,34 @@
+function Show(title, bands, date, flierImgPath, fbEventUrl){
+    let self = this;
+    let datestring = date.replace(/\./g, "/");
+
+    self.title = title;
+    self.bands = bands;
+    self.date = new Date(datestring);
+    self.dateString = date;
+    self.flierImgPath = flierImgPath;
+    self.fbEventUrl = fbEventUrl;
+
+    self.getDisplayText = function(){
+        let displayString = self.title === null ? getBandList() : self.title;
+        return '<p><a href="'+ self.fbEventUrl + '" target="_blank">' + self.dateString + ' -- ' + displayString + '</a> -- <a href="'+ self.flierImgPath + '" target="_blank">'+'flier</a></p>';
+    };
+
+    function getBandList(){
+        let bandList = '';
+        let bandlength = self.bands.length;
+        for (let i = 0; i !== bandlength; i++){
+            let bandName = self.bands[i];
+            let ending = '';
+            if (i === bandlength -2)
+                ending = ', and ';
+            else if (i === bandlength - 1)
+                ending = '';
+            else
+                ending = ', ';
+
+            bandList += bandName + ending;
+        }
+        return bandList;
+    }
+}
